e876c0d85df9d377ca54e43b466cfe70d631e8e6
Nathaniel Bunch <taranoshi@gmail.com>
2019-02-24 21:50:42 -0500

    this might fix the db connect bug

--- Project 2 - SQL Program/src/DBConnect.java
+++ Project 2 - SQL Program/src/DBConnect.java
@@ -12,10 +12,10 @@ public class DBConnect {
 
     public void setConnection(String database, String user, String password){
         //‘postgresql://cslab:TacoSh%40ck@localhost:5432/cooking'
-        String format = String.format("jdbc:postgresql://localhost:5432/%s", database);
+        //String format = String.format("jdbc:postgresql://localhost:5432/%s", database);
+        String format = String.format("jdbc:postgresql://cslab:TacoSh@ck@localhost:5432/cooking", database);
         System.out.println("Connecting to the database...");
         System.out.println(format);
-        //String format = String.format("postgresql://cslab:TacoSh@ck@localhost:5432/cooking", database);
         Properties props = new Properties();
         props.setProperty("user", user);
         props.setProperty("password", password);
