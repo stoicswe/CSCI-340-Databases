8933ee0fe9a5a3442044438ca088048b7011ca1e
Nathaniel Bunch <taranoshi@gmail.com>
2019-02-24 21:45:17 -0500

    added the port num

--- Project 2 - SQL Program/src/DBConnect.java
+++ Project 2 - SQL Program/src/DBConnect.java
@@ -12,7 +12,7 @@ public class DBConnect {
 
     public void setConnection(String database, String user, String password){
         //‘postgresql://cslab:TacoSh%40ck@localhost:5432/cooking'
-        String format = String.format("jdbc:postgresql://localhost/%s", database);
+        String format = String.format("jdbc:postgresql://localhost:5432/%s", database);
         //String format = String.format("postgresql://cslab:TacoSh@ck@localhost:5432/cooking", database);
         Properties props = new Properties();
         props.setProperty("user", user);
