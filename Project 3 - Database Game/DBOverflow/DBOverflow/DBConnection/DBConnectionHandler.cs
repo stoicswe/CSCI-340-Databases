1551d597c78d9aa75eb00e6803c23d71fe95380f
taranoshi <taranoshi@gmail.com>
2019-03-19 23:34:26 -0400

    added the third project for reAlS

---
+++ Project 3 - Database Game/DBOverflow/DBOverflow/DBConnection/DBConnectionHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace DBOverflow.DBConnection
+{
+    class DBConnectionHandler
+    {
+        private string serverName;
+        private string username;
+        private string database;
+        private NpgsqlConnection dbconnection;
+
+        public DBConnectionHandler(string server, string user, string db)
+        {
+            this.serverName = server;
+            this.username = user;
+            this.database = db;
+        }
+
+        public void Connect(string password)
+        {
+            var cond = String.Format("Host={0};Username={1};Password={2};Database={3}", serverName, username, password, database);
+            this.dbconnection = new NpgsqlConnection(cond);
+        }
+
+        public void OpenConnectoin()
+        {
+            Console.WriteLine("Connection Open to {0}@{1}", database, serverName);
+            dbconnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            Console.WriteLine("Connection Closed to {0}@{1}", database, serverName);
+            dbconnection.Close();
+        }
+
+        public string[] Query(string query)
+        {
+            Console.WriteLine("Exectuing the following query on {0}@{1}:", serverName, database);
+            Console.WriteLine(query);
+            List<string> data = new List<string>();
+            var cmd = new NpgsqlCommand(query, dbconnection);
+            var reader = cmd.ExecuteReader();
+            using (cmd)
+            using (reader)
+                while (reader.Read())
+                    data.Add(reader.GetString(0));
+            return data.ToArray();
+        }
+    }
+}
---
+++ Project 3 - Database Game/DBOverflow/DBOverflow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DBOverflow.DBConnection;
+
+namespace DBOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing DB Connection...");
+            var dbh = new DBConnectionHandler("csgpu1:5432", "cslab", "dboverflow");
+            Console.Write("Please enter password for connecting: ");
+            string p = Console.ReadLine();
+            dbh.Connect(p);
+            p = null;
+            dbh.OpenConnectoin();
+            dbh.CloseConnection();
+        }
+    }
+}
